c2a511e2d577fb40bd6e28cdb5fec7a321ff55f8
唐杰 <51323906+tangjie133@users.noreply.github.com>
2023-09-27 15:25:54 +0800

    更新 pxt.json, main.blocks, main.ts, motor.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,1 @@
-motor.controlMotor(motor.MyEnumMotor.ALL, motor.MyEnumDir.Backward, 50)
-basic.pause(1000)
-motor.controlMotorStop(motor.MyEnumMotor.ALL)
-basic.pause(1000)
-basic.forever(function () {
-	
-})
+
--- motor.ts
+++ motor.ts
@@ -23,6 +23,17 @@ namespace motor {
         //% block="backward"
         Backward,
     };
+
+    export enum Servos {
+        //% blockId="S1" block="S1"
+        S1,
+        //% blockId="S2" block="S2"
+        S2,
+        //% blockId="S3" block="S3"
+        S3,
+        //% blockId="S4" block="S4"
+        S4
+    }
     
     //% block="set %emotor direction %edir speed %speed"
     //% speed.min=0 speed.max=255
@@ -120,6 +131,52 @@ namespace motor {
                 break;
         }
     }
+    //% weight=90
+    //% blockId=servo_ServoRun block="servo|%index|angle|%angle"
+    //% angle.min=0 angle.max=360
+    export function servoRun(index: Servos, angle: number): void {
+       let data = Math.map(angle, 0, 360, 500,2550)
+        switch (index) {
+            case Servos.S1:
+                let M1Buf = pins.createBuffer(3);
+                M1Buf[0] = 0x08;
+                M1Buf[1] = data>>8 & 0xff;
+                M1Buf[2] = data & 0xff;
+                pins.i2cWriteBuffer(I2CADDR, M1Buf);
+                break;
+            case Servos.S2:
+                let M2Buf = pins.createBuffer(3);
+                M2Buf[0] = 0x0A;
+                M2Buf[1] = data >> 8 & 0xff;
+                M2Buf[2] = data & 0xff;
+                pins.i2cWriteBuffer(I2CADDR, M2Buf);
+                break;
+            case Servos.S3:
+                let M3Buf = pins.createBuffer(3);
+                M3Buf[0] = 0x0C;
+                M3Buf[1] = data >> 8 & 0xff;
+                M3Buf[2] = data & 0xff;
+                pins.i2cWriteBuffer(I2CADDR, M3Buf);
+                break;
+            case Servos.S4:
+                let M4Buf = pins.createBuffer(3);
+                M4Buf[0] = 0x0E;
+                M4Buf[1] = data >> 8 & 0xff;
+                M4Buf[2] = data & 0xff;
+                pins.i2cWriteBuffer(I2CADDR, M4Buf);
+                break;
+            default:
+                break;
+        }
+    }
 
+    //% weight=10
+    //% blockId=IR_read_version block="read voltage"
+    export function IR_read_version(): number {
+        pins.i2cWriteNumber(I2CADDR, 0x10, NumberFormat.UInt8BE);
+        let buf = pins.i2cReadBuffer(I2CADDR, 2, false);
+        let value = buf[0] << 8 | buf[1]
+        return value
+    }
 
 }
